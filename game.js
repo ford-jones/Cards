e39ca2c6c2ffcd9b820557f258192b3d971abd66
Ford Jones <fordnicholasjones@gmail.com>
2022-07-26 01:07:31 +1200

    write some psuedocode in game.js

---
+++ game.js
@@ -0,0 +1,18 @@
+// make a button that when clicked returns 5 random cards and puts them in the field
+
+//make the button clickable .onclick
+//make a randomization function
+//fetch source of card images and declare as a variable
+//when the random function has run its course through the array of cards and given back 5 cards, have them appear inside the table cells
+//this could be createElement or changing the innerHTML of the <td>
+//the tds should also have positions in an array / be numbered
+
+/*--------------VARIABLES-----------*/
+
+let clubsIndex = 0
+
+let diamondsIndex = 0
+
+let spadesIndex = 0
+
+let heartsIndex = 0
